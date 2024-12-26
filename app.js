72fe31e04ff5dc76af57d2229145e7a62fb32f1c
anonymous298 <tackletalha@gmail.com>
2024-12-26 18:01:02 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+console.log('Welcome Hello World From Talha');
+
+// age = 17;
+// price = 99.9;
+// name = 'talha';
+// entry = true;
+// inventory = null;
+// gender = undefined;
+
+// console.log(typeof age);
+// console.log(typeof price);
+// console.log(typeof name);
+// console.log(typeof entry);
+// console.log(typeof inventory);
+// console.log(typeof gender);
+
+// let age = 17;
+
+// age = 'talha';
+
+// let x = 10;
+
+// let y = x;
+
+// console.log(y);
+
+// let age = 17;
+// let fullName = 'Muhammad Talha';
+// let gender = 'Male';
+
+// let x = undefined;
+
+// console.log(age);
+// console.log(fullName);
+// console.log(gender)
+
+// console.log(x)
+
+// var number = 10;
+// var number = 20;
+// var number = 30;
+
+// console.log(number);
+
+// let x = 10;
+// x = 20;
+
+// console.log(x);
+
+// const x = 10; 
+// x = 10;
+
+// console.log(x);
+
+// const PI = 3.14159265;
+// const E = 2.718;
+
+// console.log(PI);
+// console.log(E)
+
+// let x;
+
+// x = 10 + 10
+
+// console.log(x);
+
+// const y = 10;
+
+// console.log(y)
+
+{
+    let age = 17;
+    console.log(age);
+}
+
+let age = 20;
+
+console.log(age);
