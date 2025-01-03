00c777f3fef4fff0346b68bbe7b6996dfc6f3e8a
anonymous298 <tackletalha@gmail.com>
2025-01-03 23:14:19 +0500

    learning 2

---
+++ learning - 2/script.js
@@ -0,0 +1,38 @@
+/* this is a 
+mulitline comment */
+console.log('Welcome Hello World!')
+
+//// Opeartors In JavaScript
+
+//// Arithmetic Operators
+// let a = 10;
+// let b = 5;
+
+// console.log(a, '+', b, '=', a + b);
+// console.log(a, '-', b, '=', a - b);
+// console.log(a, '*', b, '=', a * b);
+// console.log(a, '/', b, '=', a / b);
+
+// console.log(2 % 2 == 0)
+// console.log(2 ** 2)
+
+// Unary Operators
+// let a = 1;
+
+// console.log(++a);
+// console.log(a);
+
+// Assigment Operators
+
+// let a = 10;
+
+// a += 10;
+
+// console.log(a);
+
+// Comparision Operators 
+
+let a = 10;
+let b = 20;
+
+console.log(a > b);
